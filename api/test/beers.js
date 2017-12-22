5453b7c8e78887fe046782d429b44e984548ff1d
Dobbinx3 <dobbinx3@gmail.com>
2017-12-22 14:54:31 -0200

    testes

--- api/test/beers.js
+++ api/test/beers.js
@@ -1,7 +1,7 @@
 let express = require('../config/express')();
 let request = require('supertest')(express);
 
-describe('#BeersService - getAll', () => {
+describe('GET /beers', () => {
   it('#A Listagem deve ser um json', (done) => {
     request
       .get('/beers')
@@ -11,44 +11,82 @@ describe('#BeersService - getAll', () => {
   });
 });
 
-describe('#BeersService - create', () => {
+describe('POST /beers/create', () => {
   it('#Cadastro de nova cerveja sem marca', (done) => {
-    
+    request
+      .post('/beers/create')
+      .send({marca: '', titulo: 'titulo teste', descricao: 'testando mocha', preco: 22, quantidade: 200, tipo: 'Lata', volume: 355})
+      .expect(422, done);
+      
   });
 
   it('#Cadastro de nova cerveja sem titulo', (done) => {
-
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: '', descricao: 'testando mocha', preco: 22, quantidade: 200, tipo: 'Lata', volume: 355})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja sem preço', (done) => {
-
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: '', quantidade: 200, tipo: 'Lata', volume: 355})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja com o preço em formato inválido', (done) => {
-
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 'abcde', quantidade: 200, tipo: 'Lata', volume: 355})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja sem quantidade', (done) => {
-    
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: '', tipo: 'Lata', volume: 355})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja com a quantidade em formato inválido', (done) => {
-
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 'abcde', tipo: 'Lata', volume: 355})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja sem tipo', (done) => {
-
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: '', volume: 355})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja sem volume', (done) => {
-    
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: ''})
+      .expect(422, done);
   });
 
   it('#Cadastro de nova cerveja com o volume em formato inválido', (done) => {
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: 'abcd'})
+      .expect(422, done);
+  });
 
+  it('#Cadastro de nova cerveja sem descricao', (done) => {
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo sem descricao', descricao: '', preco: 123, quantidade: 200, tipo: 'Lata', volume: 250})
+      .expect(200, done);
   });
 
   it('#Cadastro de nova cerveja com dados válidos', (done) => {
-    
+    request
+      .post('/beers/create')
+      .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: 355})
+      .expect(200, done);
   });
 });
