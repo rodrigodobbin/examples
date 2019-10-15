be7c445e5eb38df188f2f680da9238a3b86e1e9a
Dobbinx3 <dobbinx3@gmail.com>
2019-10-15 19:16:48 -0300

    teste

--- api/test/beers.js
+++ api/test/beers.js
@@ -121,7 +121,7 @@ describe('PATCH /beer/:id', () => {
 
   it('#Atualiza o status de uma cerveja sem status', (done) => {
     request
-      .patch('/beer/91')
+      .patch('/beer/1')
       .set('Accept', 'application/json')
       .send({status: ''})
       .expect(400, done);
@@ -129,7 +129,7 @@ describe('PATCH /beer/:id', () => {
 
   it('#Remove uma cerveja com dados válidos', (done) => {
     request
-      .patch('/beer/91')
+      .patch('/beer/1')
       .set('Accept', 'application/json')
       .send({status: 'Inativo'})
       .expect(200, done);
@@ -139,7 +139,7 @@ describe('PATCH /beer/:id', () => {
 describe('GET /beer/:id', () => {
   it('#O retorno deve ser um json', (done) => {
     request
-      .get('/beer/91')
+      .get('/beer/1')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
@@ -147,7 +147,7 @@ describe('GET /beer/:id', () => {
 
   it('#Deve retornar apenas um resultado', (done) => {
     request
-      .get('/beer/91')
+      .get('/beer/1')
       .set('Accept', 'application/json')
       .expect(200)
       .then(response => {
@@ -168,7 +168,7 @@ describe('GET /beer/:id', () => {
 describe('PUT /beer/:id', () => {
   it('#Atualiza uma cerveja sem marca', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: '', titulo: 'titulo teste', descricao: 'testando mocha', preco: 22, quantidade: 200, tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -177,7 +177,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem titulo', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: '', descricao: 'testando mocha', preco: 22, quantidade: 200, tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -185,7 +185,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem preço', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: '', quantidade: 200, tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -193,7 +193,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja com o preço em formato inválido', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 'abcde', quantidade: 200, tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -201,7 +201,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem quantidade', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: '', tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -209,7 +209,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja com a quantidade em formato inválido', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 'abcde', tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -217,7 +217,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem tipo', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: '', volume: 355, status: 'Ativo'})
       .expect(400, done);
@@ -225,7 +225,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem volume', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: '', status: 'Ativo'})
       .expect(400, done);
@@ -233,7 +233,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja com o volume em formato inválido', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: 'abcd', status: 'Ativo'})
       .expect(400, done);
@@ -241,7 +241,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem status', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: 'abcd', status: ''})
       .expect(400, done);
@@ -249,7 +249,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja sem descricao', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo sem descricao', descricao: '', preco: 123, quantidade: 200, tipo: 'Lata', volume: 250, status: 'Ativo'})
       .expect(200, done);
@@ -257,7 +257,7 @@ describe('PUT /beer/:id', () => {
 
   it('#Atualiza uma cerveja com dados válidos', (done) => {
     request
-      .put('/beer/91')
+      .put('/beer/1')
       .set('Accept', 'application/json')
       .send({marca: 'marca teste', titulo: 'titulo teste', descricao: 'testando mocha', preco: 123, quantidade: 200, tipo: 'Lata', volume: 355, status: 'Ativo'})
       .expect(200, done);
